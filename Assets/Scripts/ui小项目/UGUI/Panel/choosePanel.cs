4989c94796e83a3a36fceb6eebdca9b6b28b0b9a
74h161 <149296270+74h161@users.noreply.github.com>
2025-11-03 23:36:33 +0800

    主界面complete

--- Assets/Scripts/ui小项目/UGUI/Panel/choosePanel.cs
+++ Assets/Scripts/ui小项目/UGUI/Panel/choosePanel.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class choosePanel :basePanel
@@ -24,6 +27,60 @@ public class choosePanel :basePanel
     {
         base.OnStart();
         UIMethod.Instance.getOrAddComponentInChild<Button>(activeobj, "back").GetComponent<Button>().onClick.AddListener(BACK);
+        Button firstMapButton = UIMethod.Instance.getOrAddComponentInChild<Button>(activeobj, "firstmap").GetComponent<Button>();
+        Button dierMapButton = UIMethod.Instance.getOrAddComponentInChild<Button>(activeobj, "secondmap").GetComponent<Button>();
+        Button disanMapButton = UIMethod.Instance.getOrAddComponentInChild<Button>(activeobj, "thirdmap").GetComponent<Button>();
+        accessMap am=playerPrefsDataMgr.Instance.loaddata(typeof(accessMap), "AccessMap") as accessMap;
+        if (am.firstIf == true)
+        {
+            
+            GameObject X = GameObject.Find("firstmap/X");
+            X.SetActive(false);
+            firstMapButton.onClick.AddListener(() =>
+            {
+                SceneManager.LoadScene("initial-map-1");
+                UIManager.Instance.pop(true);
+            });
+            
+        }
+        else
+        {
+            firstMapButton.interactable = false;
+        }
+        if (am.secondIf == true)
+        {
+           
+            GameObject X = GameObject.Find("secondmap/X");
+            X.SetActive(false);
+
+            dierMapButton.onClick.AddListener(() =>
+            {
+                SceneManager.LoadScene("Final-map-1");
+                UIManager.Instance.pop(true);
+            });
+
+        }
+        else
+        {
+           dierMapButton.interactable = false;
+        }
+        if (am.thirdIf == true)
+        {
+           
+            GameObject X = GameObject.Find("thirdmap/X");
+            X.SetActive(false);
+
+            disanMapButton.onClick.AddListener(() =>
+            {
+                SceneManager.LoadScene("Final-map-2");
+                UIManager.Instance.pop(true);
+            });
+        }
+        else
+        { 
+            disanMapButton.interactable = false;
+
+        }
 
     }
 
